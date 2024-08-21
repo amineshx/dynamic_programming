673740b3df7901a86db4c76d3df73f4c3a0089e4
amineshx <amine.charef@etu.usthb.dz>
2024-08-21 15:40:40 +0100

    Update 002.fibo.js with memoization

---
+++ 002.fibo.js
@@ -0,0 +1,15 @@
+// memoization
+//  js object , keys will be arg to fn , value will be return value
+
+
+const fibo=(n, memo={})=>{
+    if(n in memo) return memo[n]
+    if(n<=2) return 1
+    memo[n]=fibo(n-1,memo) + fibo(n-2,memo)
+    return memo[n]
+}
+
+console.log(fibo(8))    // 21
+console.log(fibo(9))    // 34
+console.log(fibo(7))    // 13
+console.log(fibo(50))   // 12586269025

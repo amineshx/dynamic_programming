0900225ab9d70f827496c2fc55fad83968a5b977
amineshx <amine.charef@etu.usthb.dz>
2024-08-22 16:20:14 +0100

    Update 005.can_sum.js and 006.can_sum.js with time and space complexity

--- 006.can_sum.js
+++ 006.can_sum.js
@@ -18,3 +18,7 @@ console.log(canSum(7,[5,3,4,7])) // true
 console.log(canSum(7,[2,4]))    // false
 console.log(canSum(8,[2,3,5]))  // true
 console.log(canSum(300,[7,14])) // false
+
+// m = targetSum    n= array length
+// O(n*m) time
+// O(m) space

bb610d583fd4d13fc557513202bc1c5f84b09feb
amineshx <amine.charef@etu.usthb.dz>
2024-08-28 03:23:40 +0100

    Update 010.bestSum.js with memoization

--- 010.bestSum.js
+++ 010.bestSum.js
@@ -1,10 +1,11 @@
-const bestSum=(targetSum, nums)=>{
+const bestSum=(targetSum, nums,memo={})=>{
+    if (targetSum in memo) return memo[targetSum]
     if (targetSum===0) return []
     if (targetSum<0) return null
     let res=null
     for (let num of nums){
         const remainder=targetSum-num
-        const remainderCombination=bestSum(remainder,nums)
+        const remainderCombination=bestSum(remainder,nums,memo)
         if (remainderCombination!==null){
             const combination=[...remainderCombination, num]
             if(res===null||combination.length<res.length){
@@ -12,6 +13,7 @@ const bestSum=(targetSum, nums)=>{
             }
         }
     }
+    memo[targetSum]=res
     return res
 }
 
@@ -20,7 +22,3 @@ console.log(bestSum(8,[2,3,5]))     //[ 5, 3 ]
 console.log(bestSum(8,[1,4,5]))     //[ 4, 4 ]
 console.log(bestSum(100,[1,2,5,25]))//[25,25,25,25]
 
-
-// m = targetSum    n= array length
-// O((n^m)*m) time
-// O(m^2) space

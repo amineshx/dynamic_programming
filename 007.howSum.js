2a4c1ea27ddac6a7af36fec830c49ecd13445a14
amineshx <amine.charef@etu.usthb.dz>
2024-08-28 01:16:05 +0100

    Update 007.howSum.js

--- 007.howSum.js
+++ 007.howSum.js
@@ -12,8 +12,12 @@ const howSum=(targetSum,nums)=>{
     return null
 }
 
-console.log(howSum(7,[2,3]))
-console.log(howSum(7,[5,3,4,7]))
-console.log(howSum(7,[2,4]))
-console.log(howSum(8,[2,3,5]))
-console.log(howSum(300,[7,14]))
+console.log(howSum(7,[2,3]))    //[ 3, 2, 2 ]
+console.log(howSum(7,[5,3,4,7])) //[ 4, 3 ]
+console.log(howSum(7,[2,4]))    //null
+console.log(howSum(8,[2,3,5]))  //[ 2, 2, 2, 2 ]
+console.log(howSum(300,[7,14])) //null
+
+// m = targetSum    n= array length
+// O(n^m  * m) time
+// O(m) space

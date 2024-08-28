ee8a78a9845677c8606ba862324181772f845ac0
amineshx <amine.charef@etu.usthb.dz>
2024-08-28 03:17:53 +0100

    Update 009.bestSum.js with time space complexity

--- 009.bestSum.js
+++ 009.bestSum.js
@@ -19,3 +19,8 @@ console.log(bestSum(7,[5,3,4,7]))   //[ 7 ]
 console.log(bestSum(8,[2,3,5]))     //[ 5, 3 ]
 console.log(bestSum(8,[1,4,5]))     //[ 4, 4 ]
 console.log(bestSum(100,[1,2,5,25]))//[25,25,25,25]
+
+
+// m = targetSum    n= array length
+// O((n^m)*m) time
+// O(m^2) space

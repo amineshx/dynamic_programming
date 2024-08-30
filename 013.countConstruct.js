808128bd768338955ca9395fee3c31028e6bcf57
amineshx <amine.charef@etu.usthb.dz>
2024-08-30 23:04:06 +0100

    Update 013.countConstruct.js with a brute force solution

---
+++ 013.countConstruct.js
@@ -0,0 +1,24 @@
+const countConstruct=(target,words)=>{
+    if(target==='') return 1
+    let res= 0
+    for(let word of words){
+        if(target.indexOf(word)===0){
+            const numWays=countConstruct(target.slice(word.length),words)
+            res+=numWays
+        }
+    }
+    return res
+}
+
+
+
+
+
+
+console.log(countConstruct("purple",["purp","p","ur","le","pupl"])) // 2
+console.log(countConstruct("abcdef",["ab","abc","cd","def","abcd"])) // 1
+console.log(countConstruct("skateboard",["bo","rd","ate","ska","sk","boar"])) //0
+console.log(countConstruct("enterapotentpot",["a","p","ent","enter","ot","o","t"])) //4
+console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+        ["e","ee","eee","eeee","eeeee","eeeeee"]
+    )) // 0
